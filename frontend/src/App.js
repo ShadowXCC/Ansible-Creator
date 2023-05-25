2ae0d017dbbbb17b8daeb73648628afaf93d8b85
ShadowXCC <shadowxcc@gmail.com>
2023-05-25 18:18:16 -0400

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { StreamLanguage } from '@codemirror/language';
+import { yaml } from '@codemirror/legacy-modes/mode/yaml';
+import { markdown } from '@codemirror/lang-markdown'
+
 import './index.css';
 import './App.css';
-import MessageView from './components/message-view';
-import MessageView2 from './components/message-view-2';
-import MessageList from './components/message-list';
+import logo from './logo.png'
 
-var test = <p>THIS IS A TEST</p>
+const header = 
+  <header id="header">
+    <nav>
+      <a href="clepore.com"><img id="menu-icon" src={ logo } alt="Company's Logo"/></a>
+      <ul>
+        <li><a href="/Home">Home</a></li>
+        <li><a href="/Pricing">Pricing</a></li>
+        <li><a href="/Voting">Voting</a></li>
+        <li><a href="/Account">Account</a></li>
+      </ul>
+    </nav>
+  </header>
+
+var defaultYAML = "---\n- name: Update web servers\n\thosts: webservers\n\tremote_user: root"
+
+const codeMirror = 
+<CodeMirror
+      value={ defaultYAML }
+      height="200px"
+      width="50%"
+      extensions={[StreamLanguage.define(yaml), markdown(yaml)]}
+    />
+
+//This was used to allow for dynamic switching of the language, this is not needed as it should always be YAML
+//import { languages } from "@codemirror/language-data"
+//markdown({codeLanguages: languages})
+
+
+
+
+
 const toRender = 
   <div>
-    <h1 className="header-red">Heading</h1>
-    {test}
-    <h3>a</h3>
-    <MessageView/>
-    <MessageView2/>
-    <MessageList/>
+    {header}
+
+    {codeMirror}
   </div>
 
 class App extends Component {
